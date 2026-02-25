c8373f18b7573b1bd8baaf902b73a603cfeb61b6
Anasyaseen92 <anasy7148@gmail.com>
2026-02-25 21:18:18 +0500

    aa

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
-    origin: ["http://localhost:5173", "https://mv92.netlify.app"]
+    origin: ["http://localhost:5173", "https://mv92.netlify.app"],
     credentials: true,
 }));
 app.use("/", express.static("uploads"));
